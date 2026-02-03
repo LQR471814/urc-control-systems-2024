4a7b09529d95a8d02136780c675dbb70a453e12d
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-02-03 12:41:49 -0800

    wip

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+  outputs = {flake-parts, ...} @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" ];
+      perSystem = {pkgs, ...}: {
+        devShells.default = (pkgs.buildFHSEnv {
+          name = "libhal-fhs";
+          targetPkgs = pkgs: with pkgs; [
+            conan
+            cmake
+            python313
+            python313Packages.pyserial
+
+            zlib
+            zstd
+            picolibc
+          ];
+          runScript = "bash";
+          profile = ''
+            export CONAN_HOME="$(pwd)/.libhal-conan2"
+          '';
+        }).env;
+      };
+    };
+}
