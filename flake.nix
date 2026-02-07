c6b3438ed08c9aeb3177e11a60fe7f190bb71c6a
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-02-07 13:59:52 -0800

    wip

--- flake.nix
+++ flake.nix
@@ -3,27 +3,52 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
-  outputs = {flake-parts, ...} @ inputs:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "aarch64-linux" ];
-      perSystem = {pkgs, ...}: {
-        devShells.default = (pkgs.buildFHSEnv {
-          name = "libhal-fhs";
-          targetPkgs = pkgs: with pkgs; [
-            conan
-            cmake
-            python313
-            python313Packages.pyserial
+  outputs =
+    { flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default =
+            (pkgs.buildFHSEnv {
+              name = "libhal-fhs";
+              targetPkgs =
+                multiPkgs: with pkgs; [
+                  conan
+                  python313
+                  python313Packages.pyserial
+                  zlib # Add this
+                  zstd # And this (often needed too)
+                  libxml2
+                  ncurses
+                  stdenv.cc.cc.lib
+                  gcc
+                  gcc.cc.lib
+                  binutils
+                  gcc-unwrapped
+                  libgcc
+                  glibc
+                  glibc.dev
+                  openssl
+                  curl
+                  xz
+                  bzip2
+                ];
+              runScript = "bash";
+              profile = ''
+                mkdir -p "$HOME/.nix-fhs-libs"
+                ln -sf /lib/libxml2.so.16 "$HOME/.nix-fhs-libs/libxml2.so.2"
+                export LD_LIBRARY_PATH="$HOME/.nix-fhs-libs:/lib:$LD_LIBRARY_PATH"
 
-            zlib
-            zstd
-            picolibc
-          ];
-          runScript = "bash";
-          profile = ''
-            export CONAN_HOME="$(pwd)/.libhal-conan2"
-          '';
-        }).env;
-      };
+                conan config install https://github.com/libhal/conan-config2.git
+                conan hal setup
+              '';
+            }).env;
+        };
     };
 }
