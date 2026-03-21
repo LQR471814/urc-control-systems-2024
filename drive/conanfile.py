a368d8429dd35d85f029bee3f26de25f574b2617
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-03-21 16:13:51 -0700

    update conanfile

--- drive/conanfile.py
+++ drive/conanfile.py
@@ -2,6 +2,7 @@ from conan import ConanFile
 
 required_conan_version = ">=2.0.14"
 
+
 class demos(ConanFile):
     python_requires = "libhal-bootstrap/[4.3.0]"
     python_requires_extend = "libhal-bootstrap.demo"
@@ -9,5 +10,5 @@ class demos(ConanFile):
     def requirements(self):
         self.requires("libhal/4.18.1")
         self.requires("libhal-util/5.8.1")
-        self.requires("libhal-arm-mcu/1.19.6")
+        self.requires("libhal-arm-mcu/1.21.1")
         self.requires("libhal-actuator/1.2.3")
