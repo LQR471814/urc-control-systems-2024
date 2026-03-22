a2b4ab845ea1c09f0b86b2cf4a729f74c9650e4b
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-03-21 19:58:40 -0700

    wip

--- drive/conanfile.py
+++ drive/conanfile.py
@@ -10,5 +10,5 @@ class demos(ConanFile):
     def requirements(self):
         self.requires("libhal/4.18.1")
         self.requires("libhal-util/5.8.1")
-        self.requires("libhal-arm-mcu/1.20.2")
+        self.requires("libhal-arm-mcu/1.21.1")
         self.requires("libhal-actuator/1.2.3")
