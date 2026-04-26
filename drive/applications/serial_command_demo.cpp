24d240b0d43b94c831f9c966086d453569e3f4da
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-04-25 18:52:57 -0700

    wip

--- drive/applications/serial_command_demo.cpp
+++ drive/applications/serial_command_demo.cpp
@@ -1,95 +1,94 @@
-#include <drivetrain.hpp>
-#include <drivetrain_math.hpp>
 #include <libhal-util/serial.hpp>
 #include <libhal-util/steady_clock.hpp>
-#include <libhal/steady_clock.hpp>
+#include <libhal/error.hpp>
+#include <libhal/units.hpp>
 #include <resource_list.hpp>
-#include <serial_commands.hpp>
+
+// #include <serial_commands.hpp>
 
 namespace sjsu::drive {
+
 void application()
 {
-  using namespace std::chrono_literals;
-
-  auto led = resources::status_led();
-  auto clock = resources::clock();
   auto console = resources::console();
-  hal::print(*console, "\nappstart\n");
-  constexpr hal::time_duration cycle_time = 250ms;
-  constexpr sec cycle_time_sec = hal_time_duration_to_sec(cycle_time);
-  auto mods_ptr = resources::swerve_modules();
-  drivetrain dt(mods_ptr, cycle_time_sec);
-  //TODO: some UB in the serial library maybe resulting in issues with the initialization of drivetrain
+  auto clock = resources::clock();
 
+  /*
   std::array cmd_defs = {
+    // This command would be called by inputting into serial:
+    //
+    // fls 2.3\n
+    //
+    // Where `fls` is the prefix, `2.3` is some float value for the first
+    // parameter, and `\n` is a newline to designate the end of a command.
+    // There can be any number of whitespaces separating the two.
+    //
+    // Deletion of command characters with the backspace character `\b` is
+    // supported, the clearing of the current command buffer with `control+C`
+    // is also supported.
     drivers::serial_commands::def{
-      .m_prefix = "set-vel",
-      .m_callback = [&](auto params) {
+      "fls",
+      [&console](auto params) {
+        if (params.size() != 1) {
+          throw hal::argument_out_of_domain(nullptr);
+        }
+        float speed = drivers::serial_commands::parse_float(params[0]);
+        hal::print<32>(*console, "Set FLS: %f\n", speed);
+      },
+    },
+    // This command would be called by inputting into serial:
+    //
+    // toggle-flash\n
+    drivers::serial_commands::def{
+      "toggle-flash",
+      [&console, &flashing](auto params) {
+        if (params.size() != 0) {
+          throw hal::argument_out_of_domain(nullptr);
+        }
+        flashing = !flashing;
+        hal::print(*console, "Toggled flashing.\n");
+      },
+    },
+    // This command would be called by inputting into serial:
+    //
+    // multiparam -4.3 4 -5\n
+    drivers::serial_commands::def{
+      "multiparam",
+      [&console](auto params) {
         if (params.size() != 3) {
           throw hal::argument_out_of_domain(nullptr);
         }
-        float x = drivers::serial_commands::parse_float(params[0]);
-        float y = drivers::serial_commands::parse_float(params[1]);
-        float w = drivers::serial_commands::parse_float(params[2]);
-        hal::print<32>(*console, "Set vel: %f,%f,%f\n", x, y, w);
-        dt.set_target_state({ { x, y }, w }, true);
+        float p1 = drivers::serial_commands::parse_float(params[0]);
+        int p2 = drivers::serial_commands::parse_int(params[1]);
+        int p3 = drivers::serial_commands::parse_int(params[2]);
+        hal::print<64>(
+          *console, "Multi-parameter command: %f %d %d\n", p1, p2, p3);
       },
     },
-    drivers::serial_commands::def{
-      .m_prefix = "hard-home",
-      .m_callback = [&](auto params) {
-        if (params.size() != 0) {
-          throw hal::argument_out_of_domain(nullptr);
-        }
-        hal::print(*console, "Homing\n");
-        dt.stop();
-        dt.hard_home();
-      },
-    },
-    drivers::serial_commands::def{
-      .m_prefix = "print-actual-states",
-      .m_callback = [&](auto params) {
-        if (params.size() != 0) {
-          throw hal::argument_out_of_domain(nullptr);
-        }
-        auto mods = resources::swerve_modules();
-        hal::print(*console, "Actual States\n");
-        for (uint8_t i = 0; i < mods->size(); i++) {
-          auto state = mods->at(i)->get_actual_state_cache();
-          hal::print<64>(*console,
-                         "mod[%i]:(%f,%f)\n",
-                         i,
-                         state.steer_angle,
-                         state.propulsion_velocity);
-        }
-      },
-    },
-    drivers::serial_commands::def{
-      .m_prefix = "print-target-states",
-      .m_callback = [&](auto params) {
-        if (params.size() != 0) {
-          throw hal::argument_out_of_domain(nullptr);
-        }
-        auto mods = resources::swerve_modules();
-        hal::print(*console, "Target States\n");
-        for (uint8_t i = 0; i < mods->size(); i++) {
-          auto state = mods->at(i)->get_target_state();
-          hal::print<64>(*console,
-                         "mod[%i]:(%f,%f)\n",
-                         i,
-                         state.steer_angle,
-                         state.propulsion_velocity);
-        }
-      },
-    }
-  };
+  };  // namespace sjsu::drive
+  */
 
-  drivers::serial_commands::handler cmd{ console };
+  // drivers::serial_commands::handler cmd{ console };
 
   while (true) {
-    cmd.handle(cmd_defs);
+    hal::print(*console, "Hello world!\n");
+    hal::delay(*clock, 250ms);
+
+    // we tradeoff responsiveness for performance, effectively batching a bunch
+    // of reads in a single 500ms interval into single read at the end of a
+    // 500ms time block
+    //
+    // to ensure that the client doesn't overflow the receive buffer in this
+    // time, we echo every character the client sends so the client can wait
+    // until we "ack" their command.
+    //
+    // this makes it awkward if your TTY client sends one character at a time
+    // instead of an entire line at a time, in these cases it makes more sense
+    // to simply reconfigure your TTY client
+    /*
     try {
-    } catch (hal::exception const& e) {
+      cmd.handle(cmd_defs);
+    } catch (hal::exception e) {
       switch (e.error_code()) {
         case std::errc::argument_out_of_domain:
           hal::print(*console, "Error: invalid argument length or type\n");
@@ -99,13 +98,7 @@ void application()
           break;
       }
     }
-    dt.periodic();
-    try {
-      dt.periodic();
-    } catch (hal::exception e) {
-      hal::print(*console, "\nexpection thown in periodic\n");
-      throw;
-    }
+    */
   }
 }
 }  // namespace sjsu::drive
